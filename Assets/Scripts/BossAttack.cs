35d6d0dbb45f0c04c19415e46132cb381c5d0234
villalbaweb <villalbaweb@gmail.com>
2020-01-12 15:52:04 -0600

    Adding Attack animation event for the Boss1 attack Animation, whenever this event is triggered we enable the special attack

--- Assets/Scripts/BossAttack.cs
+++ Assets/Scripts/BossAttack.cs
@@ -1,12 +1,15 @@
+using System.Collections;
 using UnityEngine;
 
 public class BossAttack : MonoBehaviour {
     
     // config paranms
     [SerializeField] float baseTimeBetweenAttacks = 10f;
+    [SerializeField] float specialAttackEnabledTime = 0.5f;
 
     // cache
     Animator _animator;
+    BoxCollider2D _specialAttackBoxCollider2D;
 
     // state
     float timeSincelastAttack = 0f;
@@ -14,6 +17,9 @@ public class BossAttack : MonoBehaviour {
 
     private void Start() {
         _animator = GetComponent<Animator>();
+        _specialAttackBoxCollider2D = GetComponent<BoxCollider2D>();
+
+        _specialAttackBoxCollider2D.enabled = false;
 
         timeBetweenAttacks = baseTimeBetweenAttacks;
     }
@@ -29,6 +35,18 @@ public class BossAttack : MonoBehaviour {
             _animator.SetTrigger("Attack");
         }
     }
+
+    private void AttackAnimationEvent() {
+        if (_specialAttackBoxCollider2D == null) return;
+
+        StartCoroutine(EnableSpecialAttackCollider());
+    }
+
+    IEnumerator EnableSpecialAttackCollider() {
+        _specialAttackBoxCollider2D.enabled = true;
+        yield return new WaitForSecondsRealtime(specialAttackEnabledTime);
+        _specialAttackBoxCollider2D.enabled = false;
+    } 
 
     public void CalculateTimeBetweenAttacksBasedOnHealth(int initialHealth, int remainingHealth) {
         float percentageLeft = (float)remainingHealth / (float)initialHealth;

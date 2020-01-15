49e6380e575d690f96bcd7317f901d7d20f16e60
villalbaweb <villalbaweb@gmail.com>
2020-01-14 18:30:23 -0600

    Adding Parallax Effect to Main Menu scene as test

---
+++ Assets/Scripts/Parallax.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Parallax : MonoBehaviour
+{
+    // config params
+    [SerializeField] GameObject camera;
+    [SerializeField] float parallaxEffect;
+
+    private float length, startpos;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        startpos = transform.position.x;
+        length = GetComponent<SpriteRenderer>().bounds.size.x;
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        float temp = (camera.transform.position.x * (1 - parallaxEffect));
+        float distance = (camera.transform.position.x * parallaxEffect);
+        transform.position = new Vector3(startpos + distance, transform.position.y, transform.position.z);
+        if (temp > startpos + length) 
+        {
+            startpos += length;
+        }
+        else if (temp < startpos - length) 
+        {
+            startpos -= length;
+        }
+    }
+}

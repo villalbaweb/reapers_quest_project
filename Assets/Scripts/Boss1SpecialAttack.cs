6cc5af8f7ebffd0f72b863ace9e3aa3b66832d1c
villalbaweb <villalbaweb@gmail.com>
2020-01-16 17:44:04 -0600

    Refactoring Boss 1 prefab, removed the box collider used to attack the player

--- Assets/Scripts/Boss1SpecialAttack.cs
+++ Assets/Scripts/Boss1SpecialAttack.cs
@@ -8,27 +8,15 @@ public class Boss1SpecialAttack : MonoBehaviour {
     [SerializeField] GameObject explosionParticleEffect = null;
     [SerializeField] Transform explosionParticleSystemPosition = null;
 
-    // cache
-    BoxCollider2D _specialAttackBoxCollider2D;
-    
-    private void Start() {
-        _specialAttackBoxCollider2D = GetComponent<BoxCollider2D>();
-        _specialAttackBoxCollider2D.enabled = false;
-    }
-
     private void AttackAnimationEvent()
     {
-        if (_specialAttackBoxCollider2D == null) return;
-        
         StartCoroutine(EnableSpecialAttackCollider());
     }
 
     IEnumerator EnableSpecialAttackCollider()
     {
         GameObject explosionParticles = Instantiate(explosionParticleEffect, explosionParticleSystemPosition.position, Quaternion.identity);
-        _specialAttackBoxCollider2D.enabled = true;
         yield return new WaitForSecondsRealtime(specialAttackEnabledTime);
         Destroy(explosionParticles);
-        _specialAttackBoxCollider2D.enabled = false;
     }
 }

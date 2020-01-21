82d8bff2b84a548eb528e6af13f3cb1e9facf746
villalbaweb <villalbaweb@gmail.com>
2020-01-20 18:20:58 -0600

    Adding SFX to Boss1 special attack

--- Assets/Scripts/Boss1SpecialAttack.cs
+++ Assets/Scripts/Boss1SpecialAttack.cs
@@ -7,13 +7,22 @@ public class Boss1SpecialAttack : MonoBehaviour {
     [SerializeField] float specialAttackEnabledTime = 0.5f;
     [SerializeField] GameObject explosionParticleEffect = null;
     [SerializeField] Transform explosionParticleSystemPosition = null;
+    [SerializeField] AudioClip attackSFX = null;
+    [SerializeField] float audioSFXVolume = 0.3f;
+
+    private void AttackSFXPlayAnimationEvent()
+    {
+        if (!attackSFX) { return; }
+
+        AudioSource.PlayClipAtPoint(attackSFX, Camera.main.transform.position, audioSFXVolume);
+    }
 
     private void AttackAnimationEvent()
     {
-        StartCoroutine(EnableSpecialAttackCollider());
+        StartCoroutine(SpecialAttack());
     }
 
-    IEnumerator EnableSpecialAttackCollider()
+    IEnumerator SpecialAttack()
     {
         GameObject explosionParticles = Instantiate(explosionParticleEffect, explosionParticleSystemPosition.position, Quaternion.identity);
         yield return new WaitForSecondsRealtime(specialAttackEnabledTime);

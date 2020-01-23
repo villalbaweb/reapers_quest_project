87269c5a3fce306469d7e8798215150fd5844e1d
villalbaweb <villalbaweb@gmail.com>
2020-01-22 23:47:37 -0600

    same

---
+++ Assets/Scripts/BossFightMusic.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BossFightMusic : MonoBehaviour
+{
+    // cache
+    GameMusic _gameMusic; 
+
+    private void Start() {
+        _gameMusic = FindObjectOfType<GameMusic>();
+
+        _gameMusic.PlayBossBattleMusic();
+    }
+
+    private void OnDestroy() {
+        _gameMusic.PlayRegularGameMusic();
+    }
+
+}
---
+++ Assets/Scripts/GameMusic.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameMusic : MonoBehaviour
+{
+    // config props
+    [SerializeField] AudioClip gameMusic;
+    [SerializeField] AudioClip bossBattleMusic;
+
+    // cache
+    AudioSource _musicAudioSource;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _musicAudioSource = GetComponent<AudioSource>();
+
+        PlayRegularGameMusic();
+    }
+
+    public void PlayRegularGameMusic()
+    {
+        _musicAudioSource.Stop();
+        _musicAudioSource.clip = gameMusic;
+        _musicAudioSource.Play();
+    }
+
+    public void PlayBossBattleMusic()
+    {
+        _musicAudioSource.Stop();
+        _musicAudioSource.clip = bossBattleMusic;
+        _musicAudioSource.Play();
+    }
+
+}

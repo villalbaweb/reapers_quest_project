ea56e42907d52b63d9d88e73a146a449d5f6289e
villalbaweb <villalbaweb@gmail.com>
2020-01-29 22:52:27 -0600

    Adding Moving Platform object, now the Player changes its transform parent to be the moving platform's transform

---
+++ Assets/Scripts/MovingPlatform.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+public class MovingPlatform : MonoBehaviour
+{
+    private void OnTriggerEnter2D(Collider2D other) 
+    {
+        other.transform.SetParent(transform);
+    }
+
+    private void OnTriggerExit2D(Collider2D other) 
+    {
+        other.transform.SetParent(null);   
+    }
+}

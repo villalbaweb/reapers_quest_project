1c2dd9167f50b79ad677cf980b4dccf23a7cb44b
villalbaweb <villalbaweb@gmail.com>
2020-01-31 16:07:52 -0600

    Adding movement to the platforms and create a new Moving Platform Set prefab that pretty much offers everything required to create a moving platform

--- Assets/Scripts/MovingPlatform.cs
+++ Assets/Scripts/MovingPlatform.cs
@@ -2,6 +2,34 @@
 
 public class MovingPlatform : MonoBehaviour
 {
+    // config
+    [SerializeField] MovingPlatformPath movingPath = null;
+    [SerializeField] float waypointTolerance = 0.1f;
+    [SerializeField] float speed = 1f;
+
+    // state
+    int movingPathWaypointIndex = 0;
+
+    private void Update() 
+    {
+        if(movingPath != null) {
+            if (AtWaypoint()) {
+                movingPathWaypointIndex = movingPath.NextPosition(movingPathWaypointIndex);
+            }
+        }
+
+        float step = speed * Time.deltaTime;
+
+        // move sprite towards the target location
+        transform.position = Vector2.MoveTowards(transform.position, movingPath.GetPathpoint(movingPathWaypointIndex), step);
+    }
+
+    private bool AtWaypoint()
+    {
+        float distanceToWaypoint = Vector2.Distance(transform.position, movingPath.GetPathpoint(movingPathWaypointIndex));
+        return distanceToWaypoint < waypointTolerance;
+    }
+
     private void OnTriggerEnter2D(Collider2D other) 
     {
         other.transform.SetParent(transform);
--- Assets/Scripts/MovingPlatformPath.cs
+++ Assets/Scripts/MovingPlatformPath.cs
@@ -2,14 +2,15 @@
 
 public class MovingPlatformPath : MonoBehaviour
 {
+
     const float gizmoSize = 0.3f;
 
     private void OnDrawGizmos()
     {
         for (int i = 0; i < transform.childCount; i++)
         {
-            Vector3 gizmoPosition = GetPathpoint(i);
-            Vector3 nextGizmoPosition = GetPathpoint(NextPosition(i));
+            Vector2 gizmoPosition = GetPathpoint(i);
+            Vector2 nextGizmoPosition = GetPathpoint(NextPosition(i));
 
             Gizmos.color = Color.cyan;
             Gizmos.DrawSphere(gizmoPosition, gizmoSize);
@@ -24,7 +25,7 @@ public class MovingPlatformPath : MonoBehaviour
             : i + 1;
     }
 
-    public Vector3 GetPathpoint(int i)
+    public Vector2 GetPathpoint(int i)
     {
         return transform.GetChild(i).position;
     }

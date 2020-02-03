21e94d9f2fddaaa85b3a6d85de0fb04c1e8a53ae
villalbaweb <villalbaweb@gmail.com>
2020-02-03 17:22:28 -0600

    Adding new swinging obstacles prefabs

---
+++ Assets/Scripts/Swing.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Swing : MonoBehaviour
+{
+    // config props
+    [SerializeField] float initialSwingForce = 3000f;
+
+    // cache
+    Rigidbody2D _bladeRigidBody;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _bladeRigidBody = GetComponent<Rigidbody2D>();
+
+        Vector2 applyForce = new Vector2(initialSwingForce, _bladeRigidBody.position.y);
+        _bladeRigidBody.AddForce(applyForce);
+    }
+}

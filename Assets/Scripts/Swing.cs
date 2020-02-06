d869a479ccfced571bb6bca1172937204aef10c7
villalbaweb <villalbaweb@gmail.com>
2020-02-06 16:35:02 -0600

    Adding EnemyHealth script to swinggers that way they can be destroyed by the Reaper

--- Assets/Scripts/Swing.cs
+++ Assets/Scripts/Swing.cs
@@ -1,21 +1,30 @@
-﻿using System.Collections;
-using System.Collections.Generic;
+﻿using System;
 using UnityEngine;
 
-public class Swing : MonoBehaviour
+[RequireComponent(typeof(EnemyHealth))]
+public class Sswing : MonoBehaviour
 {
     // config props
     [SerializeField] float initialSwingForce = 100f;
 
     // cache
     Rigidbody2D _bladeRigidBody;
+    EnemyHealth _enemyHealth;
 
     // Start is called before the first frame update
     void Start()
     {
         _bladeRigidBody = GetComponent<Rigidbody2D>();
+        _enemyHealth = GetComponent<EnemyHealth>();
+
+        _enemyHealth.OnDie += OnSwingerDie;
 
         Vector2 applyForce = new Vector2(initialSwingForce, _bladeRigidBody.position.y);
         _bladeRigidBody.AddForce(applyForce, ForceMode2D.Impulse);
+    }
+
+    private void OnSwingerDie()
+    {
+        Destroy(gameObject);
     }
 }

5d715ae06411b380aca38d944142e584cb0f8efb
villalbaweb <villalbaweb@gmail.com>
2020-02-06 17:53:00 -0600

    Adding a SwingerObstaclesController that handles the behavior

--- Assets/Scripts/Swing.cs
+++ Assets/Scripts/Swing.cs
@@ -1,30 +1,17 @@
-﻿using System;
-using UnityEngine;
+﻿using UnityEngine;
 
-[RequireComponent(typeof(EnemyHealth))]
 public class Swing : MonoBehaviour
 {
     // config props
     [SerializeField] float initialSwingForce = 100f;
 
     // cache
-    Rigidbody2D _bladeRigidBody;
-    EnemyHealth _enemyHealth;
+    Rigidbody2D _swinggerRigidBody;
 
-    // Start is called before the first frame update
-    void Start()
+    public void StartSwing()
     {
-        _bladeRigidBody = GetComponent<Rigidbody2D>();
-        _enemyHealth = GetComponent<EnemyHealth>();
-
-        _enemyHealth.OnDie += OnSwingerDie;
-
-        Vector2 applyForce = new Vector2(initialSwingForce, _bladeRigidBody.position.y);
-        _bladeRigidBody.AddForce(applyForce, ForceMode2D.Impulse);
-    }
-
-    private void OnSwingerDie()
-    {
-        Destroy(gameObject);
+        _swinggerRigidBody = GetComponent<Rigidbody2D>();
+        Vector2 applyForce = new Vector2(initialSwingForce, _swinggerRigidBody.position.y);
+        _swinggerRigidBody.AddForce(applyForce, ForceMode2D.Impulse);
     }
 }
---
+++ Assets/Scripts/SwingerObstacleController.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(EnemyHealth), typeof(Swing))]
+public class SwingerObstacleController : MonoBehaviour
+{
+    // cache
+    EnemyHealth _enemyHealth;
+    Swing _swing;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _enemyHealth = GetComponent<EnemyHealth>();
+        _swing = GetComponent<Swing>();
+        
+        _enemyHealth.OnDie += OnDestroySwinger;
+        
+        _swing.StartSwing();
+    }
+
+    private void OnDestroy() 
+    {
+        _enemyHealth.OnDie -= OnDestroySwinger;    
+    }
+
+    private void OnDestroySwinger()
+    {
+        Destroy(gameObject);
+    }
+}

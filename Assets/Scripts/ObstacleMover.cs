1b94ddcd998b3460dcdf816b9ce7bc4820dd8415
villalbaweb <villalbaweb@gmail.com>
2020-02-25 21:56:35 -0600

    Adding Rotating Blade prefab

---
+++ Assets/Scripts/ObstacleMover.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+public class ObstacleMover : MonoBehaviour
+{
+    // config params
+    [SerializeField] bool moveRight = true;
+    [SerializeField] float speed = 2f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        Move();
+    }
+
+    private void Move() 
+    {
+        Vector2 directionVector = moveRight ? Vector2.right : Vector2.left;
+
+        transform.Translate(directionVector * speed * Time.deltaTime, Space.World);    
+    }
+}
---
+++ Assets/Scripts/Rotator.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+public class Rotator : MonoBehaviour
+{
+    // config params
+    [SerializeField] [Range(-7200f, 7200f)] float rotation = 3600f;
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(Vector3.forward, rotation * Time.deltaTime, Space.World);
+    }
+}

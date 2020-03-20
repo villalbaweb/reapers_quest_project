a84284d55cdca28b848073614f36ecbfdd34b82c
villalbaweb <villalbaweb@gmail.com>
2020-03-19 23:15:19 -0600

    Saving RotatingBladeController script

---
+++ Assets/Scripts/RotatingBladeController.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(EnemyHealth))]
+public class RotatingBladeController : MonoBehaviour
+{
+    // cache
+    EnemyHealth _enemyHealth;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _enemyHealth = GetComponent<EnemyHealth>();
+
+        _enemyHealth.OnDie += OnDestroySwinger;
+    }
+
+    private void OnDestroy()
+    {
+        _enemyHealth.OnDie -= OnDestroySwinger;
+    }
+
+    private void OnDestroySwinger()
+    {
+        Destroy(gameObject);
+    }
+}

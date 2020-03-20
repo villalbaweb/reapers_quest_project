2bd0a843fb0fac8592ccffe18b8dd2830493f66b
villalbaweb <villalbaweb@gmail.com>
2020-03-19 23:26:01 -0600

    Adding a Game Object that will work as container for the Boss2 projectiles

--- Assets/Scripts/Boss2SpecialAttack.cs
+++ Assets/Scripts/Boss2SpecialAttack.cs
@@ -5,9 +5,31 @@ public class Boss2SpecialAttack : MonoBehaviour
     // config params
     [SerializeField] GameObject projectile = null;
 
+    // State
+    GameObject _projectileParent;
+
+    const string PROJECTILE_PARENT_NAME = "Boss2 Projectiles";
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        CreateProjectileParent();
+    }
+
+    private void CreateProjectileParent()
+    {
+        _projectileParent = GameObject.Find(PROJECTILE_PARENT_NAME);
+
+        if (!_projectileParent)
+        {
+            _projectileParent = new GameObject(PROJECTILE_PARENT_NAME);
+        }
+    }
     private void FireBallAttackAnimationEvent()
     {
         Follower followerObject = Instantiate(projectile, transform.position, Quaternion.identity).GetComponent<Follower>();
         followerObject.target = FindObjectOfType<Player>().transform;
+
+        followerObject.transform.parent = _projectileParent.transform;
     }
 }

ef64dd0abe37d8742516cf686a37b7ab31c48bbd
villalbaweb <villalbaweb@gmail.com>
2020-03-30 21:41:13 -0600

    Now the reward items get randomly spawned

--- Assets/Scripts/RewardSpawner.cs
+++ Assets/Scripts/RewardSpawner.cs
@@ -5,10 +5,11 @@ public class RewardSpawner : MonoBehaviour
     // config params
     [SerializeField] GameObject rewardItem = null;
     [SerializeField] int numberOfItems = 10;
+    [SerializeField] float maxXForceToSpawn = 10f;
+    [SerializeField] float maxYForceToSpawn = 5f;
 
     public void StartSpawn()
     {
-        print($"number of items {numberOfItems} of {rewardItem.name}");
         for (int i = 0; i < numberOfItems; i++)
         {
             InstantiateItem();
@@ -17,6 +18,10 @@ public class RewardSpawner : MonoBehaviour
 
     private void InstantiateItem()
     {
-        Instantiate(rewardItem, transform.position, Quaternion.identity);
+        GameObject item = Instantiate(rewardItem, transform.position, Quaternion.identity);
+
+        Vector2 forceToAdd = new Vector2(Random.Range(-maxXForceToSpawn, maxXForceToSpawn), Random.Range(1, maxYForceToSpawn));
+
+        item.GetComponent<Rigidbody2D>().AddForce(forceToAdd, ForceMode2D.Impulse);
     }
 }

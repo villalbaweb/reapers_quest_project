a1a7a98e611fff3d4eb126013901ebc2b1480ac8
villalbaweb <villalbaweb@gmail.com>
2020-03-30 20:17:30 -0600

    On Boss levels the ExitLevel game object will be disabled until the boss is defeated

---
+++ Assets/Scripts/LevelExitEnabler.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+
+public class LevelExitEnabler : MonoBehaviour
+{
+    //cache
+    EnemyHealth _enemyHealth;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        gameObject.SetActive(false);
+
+        BossController bossGameObject = FindObjectOfType<BossController>();
+        _enemyHealth = bossGameObject?.GetComponent<EnemyHealth>();
+
+        SubscribeHealthEvents();
+    }
+
+    private void OnDieEvent()
+    {
+        gameObject.SetActive(true);
+        UnsubscribeHealthEvents();
+    }
+
+    private void SubscribeHealthEvents()
+    {
+        if (!_enemyHealth) return;
+
+        _enemyHealth.OnDie += OnDieEvent;
+    }
+
+    private void UnsubscribeHealthEvents()
+    {
+        if (!_enemyHealth) return;
+
+        _enemyHealth.OnDie -= OnDieEvent;
+    }
+}

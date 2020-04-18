0e05dca44030fabfa7223e2c72496d61100a41f0
villalbaweb <villalbaweb@gmail.com>
2020-04-18 03:00:03 -0500

    Now the pause button change between pause and continue sprites

--- Assets/Scripts/PauseButtonController.cs
+++ Assets/Scripts/PauseButtonController.cs
@@ -1,17 +1,37 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
 
 public class PauseButtonController : MonoBehaviour
 {
+    // config params
+    [SerializeField] Sprite pauseSprite = null;
+    [SerializeField] Sprite continueSprite = null;
+
+    // state
     private bool isPaused = false;
+
+    // cache
+    Image _image;
+
+    private void Start() 
+    {
+        _image = GetComponent<Image>();    
+    }
 
     public void PauseControl()
     {
         isPaused = !isPaused;
 
         if (isPaused)
+        {
+            _image.sprite = continueSprite;
             Time.timeScale = 0;
+        }
         else
+        {
+            _image.sprite = pauseSprite;
             Time.timeScale = 1;
+        }
 
     }
 }

297b3ea562398d2822e3708f119783e67004c7cf
villalbaweb <villalbaweb@gmail.com>
2020-04-18 02:43:09 -0500

    Adding Pause Button and the PauseButtonController that starts/ stops pause and will launch the menu

---
+++ Assets/Scripts/PauseButtonController.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+public class PauseButtonController : MonoBehaviour
+{
+    private bool isPaused = false;
+
+    public void PauseControl()
+    {
+        isPaused = !isPaused;
+
+        if (isPaused)
+            Time.timeScale = 0;
+        else
+            Time.timeScale = 1;
+
+    }
+}

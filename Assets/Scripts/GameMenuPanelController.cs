4b14183bd500eeaaea3763120c37c57467cbc7e0
villalbaweb <villalbaweb@gmail.com>
2020-04-19 18:51:09 -0500

    Now when enabling the Menu Panel the control buttons, joystick got inactive

--- Assets/Scripts/GameMenuPanelController.cs
+++ Assets/Scripts/GameMenuPanelController.cs
@@ -2,19 +2,35 @@
 
 public class GameMenuPanelController : MonoBehaviour
 {
+    // cache
+    GameObject[] _gameControls;
+
     // Start is called before the first frame update
     void Start()
     {
+        _gameControls = GameObject.FindGameObjectsWithTag("GameController");
+
         Disable();
     }
 
     public void Enable() 
     {
+        EnableDisableGameControls(false);
         gameObject.SetActive(true);    
     }
 
     public void Disable()
     {
+        EnableDisableGameControls(true);
         gameObject.SetActive(false);
+    }
+
+    private void EnableDisableGameControls(bool isEnable)
+    {
+        foreach(GameObject control in _gameControls)
+        {
+            // print($"{control.name}");
+            control.SetActive(isEnable);
+        }
     }
 }

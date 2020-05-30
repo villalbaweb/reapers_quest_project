d12b929482bdb0be4c982dbba352e60c22878511
villalbaweb <villalbaweb@gmail.com>
2020-05-29 23:19:57 -0500

    Adding scripts to idenitfy and to control de PowerUp particle system

---
+++ Assets/Scripts/PlayerPowerupController.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+public class PlayerPowerupController : MonoBehaviour
+{
+    [SerializeField] bool IsPowerupEnabled = false;
+
+    PowerUpParticleSystem _powerUpParticleSystem;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _powerUpParticleSystem = FindObjectOfType<PowerUpParticleSystem>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        _powerUpParticleSystem?.gameObject.SetActive(IsPowerupEnabled);
+    }
+}

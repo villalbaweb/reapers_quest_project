ed2929281ad472c4561a1020401fe7823b4ce765
villalbaweb <villalbaweb@gmail.com>
2020-05-30 16:07:55 -0500

    Now when the Powerup is on the enemies die on touch

--- Assets/Scripts/PowerUpParticleSystem.cs
+++ Assets/Scripts/PowerUpParticleSystem.cs
@@ -2,5 +2,15 @@
 
 public class PowerUpParticleSystem : MonoBehaviour
 {
+    //config params
+    [SerializeField] int powerUpDamage = 1000;
 
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        EnemyHealth enemyHealth = collision.gameObject.GetComponent<EnemyHealth>();
+        if (enemyHealth)
+        {
+            enemyHealth.TakeDamage(powerUpDamage);
+        }
+    }
 }

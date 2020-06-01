c228c78593cf7022077a66700a33024df1aa8e84
villalbaweb <villalbaweb@gmail.com>
2020-05-31 23:03:24 -0500

    Now the instructions will appear 1 second after the title vanishes

---
+++ Assets/Scripts/GameInstructionsController.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+
+public class GameInstructionsController : MonoBehaviour
+{
+    // cache 
+    Timer _timer;
+    InstructionComponent[] _instructions;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _timer = GetComponent<Timer>();
+
+        _timer.OnFinished += ShowInstructions;
+    }
+
+    private void OnDestroy() {
+        _timer.OnFinished -= ShowInstructions;
+    }
+
+    void ShowInstructions()
+    {
+        _instructions = Resources.FindObjectsOfTypeAll<InstructionComponent>();
+
+        foreach(InstructionComponent instruction in _instructions)
+        {
+            instruction.gameObject.SetActive(true);
+        }
+    }
+}
